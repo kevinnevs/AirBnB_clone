f7dab8d33e2e8e914aeca73698611e4a54d88053
kevinnevs <kevinnjuguna2@gmail.com>
2022-10-31 15:31:25 +0000

    fixing pycodestyle, unittest

--- models/base_model.py
+++ models/base_model.py
@@ -21,11 +21,13 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != '__class__':
                     setattr(self, key, value)
+
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
             models.storage.new(self)
+
     def __str__(self):
         """
         string representation of base model
